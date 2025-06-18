8fbf64f5ba07d697fbbaefce8ebcbf7417d331a6
Priyanka katore <priyankakatore12@gmail.com>
2025-06-18 11:56:02 +0530

    Emp controller

--- src/model/empcrudmodel.js
+++ src/model/empcrudmodel.js
@@ -1,7 +1,7 @@
 let conn=require("../../db.js");
 exports.saveEmployee = (...empData) => {
   return new Promise((resolve, reject) => { 
-    conn.query("INSERT INTO employee (eid, name, mail, contact, sal, photo, deptid) VALUES ('0',?, ?, ?, ?, ?, ?)", [...empData], (err, result) => {
+    conn.query("insert into employee (eid, name, mail, contact, sal, photo, deptid) values ('0',?, ?, ?, ?, ?, ?)", [...empData], (err, result) => {
       if (err) {
         reject("not save" + err);
       } else {
